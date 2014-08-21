5182b81e50268aa73ed6395e8095163eca9b1773
iteles <code@inesteles.com>
2014-08-21 20:34:16 +0100

    First pass at checking availability of a hard-coded Instagram username using request

---
+++ Professional-Node-js-exercises/basic-http-request-for-instagram-username.js
@@ -0,0 +1,11 @@
+//first step of http request for IG username availability, using request module
+var request = require('request');
+
+request('http://www.instagram.com/ajkjhdiuy', function (error, response, body) {
+    if(!error && response.statusCode == 404){
+    console.log('Yay! That username is available on Instagram.');
+  }
+  else{
+    console.log('Sorry, that username is already taken on Instagram.');
+  }
+});
