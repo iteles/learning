bc513e0f45b7fe33da19f54c5e1104611a28f842
iteles <code@inesteles.com>
2014-11-06 19:39:37 +0000

    Added dynamic route using id

--- meteor-discover-meteor/lib/router.js
+++ meteor-discover-meteor/lib/router.js
@@ -11,3 +11,11 @@ Router.configure(
 Router.route('/',
   {name: 'postsList'}
 );
+
+Router.route('/:_id',
+//the '_id' here goes into the router's params array
+  {name: 'postPage',
+    //thi data context passes the id in the current URL to the findOne function
+   data: function(){ return Posts.findOne(this.params._id); }
+   }
+);
