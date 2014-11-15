0419e03139b8c82a30f3cd8d10faba1fcb290aa2
iteles <code@inesteles.com>
2014-11-15 22:58:51 +0000

    Removed insecure package and changed permissions using Collectionname.allow()

--- meteor-discover-meteor/lib/collections/posts.js
+++ meteor-discover-meteor/lib/collections/posts.js
@@ -1,1 +1,8 @@
 Posts = new Mongo.Collection("posts");
+
+//Posts.allow added in after insecure package was removed
+Posts.allow({
+insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId; }
+});
