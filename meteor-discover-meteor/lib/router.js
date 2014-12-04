075b4b4bb6a3caf2db58c4a3050e7894ab81b073
iteles <code@inesteles.com>
2014-12-04 23:05:35 +0000

    Adds basic comment data

--- meteor-discover-meteor/lib/router.js
+++ meteor-discover-meteor/lib/router.js
@@ -3,7 +3,7 @@ Router.configure(
     layoutTemplate: 'layout',
     loadingTemplate: 'loading',
     waitOn: function(){
-      return Meteor.subscribe('posts');
+      return [Meteor.subscribe('posts'), Meteor.subscribe('comments')];
     }
   }
 );
