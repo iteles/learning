bb1f233ee7a9aafffc297c2678da53eea239bc8c
iteles <code@inesteles.com>
2014-12-06 17:52:12 +0000

    Introduced comment count

--- meteor-discover-meteor/client/templates/posts/post_item.js
+++ meteor-discover-meteor/client/templates/posts/post_item.js
@@ -2,7 +2,7 @@
 
 //returns {{domain}} and {{url}} for postItem template
 Template.postItem.helpers({
-  //new attribute created here to determine whether the current user is the owner/author of the post
+  //new attribute created here, returns true if the user who owns the post is equal to the currently logged in user
   ownPost: function(){
     return this.userId === Meteor.userId();
   },
@@ -11,8 +11,11 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;  //returns full URL, e.g. http://sachagreif.com/introducing-telescope/
     return a.hostname; //returns just domain, e.g sachagreif.com
-
     //essentially domain = a.hostname & a.href=this.url
+  },
+
+  commentCount: function(){
+    return Comments.find(this._id).count();
   }
 });
 
