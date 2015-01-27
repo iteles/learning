21017da72f8487ae0d3d6884622aa359ca64b672
iteles <code@inesteles.com>
2015-01-27 12:05:49 +0000

    Unsuccesful at test passing so far but structure of testing, watching it fail, write minimum code for it to pass understood

--- testing-with-nodejs/lib/tags.js
+++ testing-with-nodejs/lib/tags.js
@@ -1,5 +1,29 @@
 var exports = module.exports = {};
 
-exports.parse = function(){
-  //code for parse() goes here
+exports.parse = function(args){
+  var options = {};
+
+  //set to defaults if the item doesn't exist in the args array
+  if (typeof defaults === "object" && !(defaults instanceof Array)) {
+    options = defaults;
+  }
+
+  for (var i in args) { //Cycle through args
+    var arg = args[i];
+    //Check if Long formed tag by checking first 2
+    //characters are '--'
+    if (arg.substr(0, 2) === "--") {
+      console.log('Im inside the if');
+      arg = arg.substr(2);
+      //if the remaining arg starts with a '='
+      if (arg.indexOf("=") !== -1) {
+        //splits the string into an array of strings at the '='
+        arg = arg.split("=");
+        //returns the value at the 0th index and removes it from the array
+        var key = arg.shift();
+        options[key] = arg.join("=");
+      }
+    }
+  }
+  return options;
 };
--- testing-with-nodejs/test/tagsSpec.js
+++ testing-with-nodejs/test/tagsSpec.js
@@ -8,12 +8,26 @@ describe('Tags', function(){
   describe('parse()', function(){
     //we are testing the parse function works correctly with long form tags
     it('should parse long form tags', function(){
-      var args = ('--depth=2', '--hello=world');
+      var args = ('--depth=4', '--hello=world');
       var results = tags.parse(args);
 
       //expect results to have a property depth of 4
       expect(results).to.have.a.property('depth', 4);
       expect(results).to.have.a.property('hello', 'world');
+    });
+
+    it('should fallback to defaults', function(){
+      var args = ('--depth=2', '--hello=world');
+      var defaults = {depth:2, foo:"bar"};
+      var results = tags.parse(args, defaults);
+
+      var expected = {
+        depth:4,
+        foo: "bar",
+        hello: "world"
+      };
+
+      expect(results).to.deep.equal(expected);
     });
   });
 });
