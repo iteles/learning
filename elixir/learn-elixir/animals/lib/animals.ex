f63cd86840040ef2e941cc010497ba57468db298
iteles <ines+github@dwyl.com>
2019-09-20 14:55:36 +0100

    Sorts out compile errors in iex

--- elixir/learn-elixir/animals/lib/animals.ex
+++ elixir/learn-elixir/animals/lib/animals.ex
@@ -1,17 +1,17 @@
 defmodule Animals do
   @moduledoc """
-  Documentation for animals
+  Documentation for Animals.
   """
 
   @doc """
   create_zoo returns a list of animals you would find in a zoo
 
   ## Examples
-    iex> Animals.create_zoo
-    ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
-
+      iex> Animals.create_zoo
+      ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
   """
-    def create_zoo do
+
+  def create_zoo do
     ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
   end
 
@@ -20,20 +20,28 @@ defmodule Animals do
   the same elements as the first.
 
   ## Examples
-
-      iex> zoo = Animals.create_zoo
-      iex> Animals.randomise(zoo)
-      ["monkey", "tiger", "elephant", "gorilla", "giraffe", "lion"]
-
+    iex> zoo = Animals.create_zoo
+    iex> Animals.randomise(zoo)
+    ["monkey", "tiger", "elephant", "gorilla", "giraffe", "lion"]
   """
+
   def randomise(zoo) do
     Enum.shuffle(zoo)
   end
 
-  @doc """"
-  contains? takes in a list of animals at the zoo and a single animal, returning
-  a bool that will tell you whether than single animal exists in your list.
+  @doc """
+  contains? takes a list of zoo animals and a single animal and returns a boolean
+  as to whether or not the list contains the given animal.
 
+  ## Examples
+      iex> zoo = Animals.create_zoo
+      iex> Animals.contains?(zoo, "gorilla")
+      true
+  """
+
+  def contains?(zoo, animal) do
+    Enum.member?(zoo, animal)
+  end
 
 
 end
